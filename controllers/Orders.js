be92407280c5996a6496ca38c3eaf67d25f07740
AldiNrh <laevetian@gmail.com>
2019-07-24 05:57:17 +0700

    modified Orders module

--- controllers/Orders.js
+++ controllers/Orders.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
 const Orders = require('../models/Orders');
 
 module.exports.getAllOrders = (req, res) =>{
@@ -9,11 +12,23 @@ module.exports.getAllOrders = (req, res) =>{
 	})
 }
 
-module.exports.getOrdersByUserId = (req, res) =>{
-	Orders.findAll({ where: { userId: req.params.userId } })
-	.then(orders => {
-		res.json(orders)
-	}).then(orders => {
-		res.json(categorys)
+module.exports.postOrders = (req, res) =>{
+		jwt.verify(req.token, process.env.SECRETKEY, (error,authData)=>{
+				if (error) {
+						res.sendStatus(403);
+				}else{
+					if (authData['roles']=="user") {
+						var bookId = req.body.bookId;
+						Orders.create({
+								userId: authData['id'],
+								bookId : bookId
+						})
+						.then(orders => {
+								res.json(orders);
+						});
+				}else{
+					res.sendStatus(403);
+				}
+			}
 	});
 }
--- routes/Orders.js
+++ routes/Orders.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const auth = require('../configs/auth');
 
 const router = express.Router();
 
 const ordersController = require('../controllers/Orders');
 
-router.get('/', keranjangController.getAllOrders);
+router.get('/', ordersController.getAllOrders);
+router.post('/',auth.verifyToken, ordersController.postOrders);
 
 module.exports = router;
